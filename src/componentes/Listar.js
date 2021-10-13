158c94b85110dd5de0fdba714d385b0402138649
Ernesto V <evalle.mx@gmail.com>
2021-10-12 21:40:55 -0400

    12) Cargando datos, uso de state

--- src/componentes/Listar.js
+++ src/componentes/Listar.js
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 class Listar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { datosCargados:false }
     }
 
 
     cargarDatos(){
 
-        //fetch("https://jsonplaceholder.typicode.com/users")
-        fetch("https://api.dailymotion.com/videos?channel=sport&limit=10")
+        fetch("https://jsonplaceholder.typicode.com/users")
+        // fetch("https://api.dailymotion.com/videos?channel=sport&limit=10")
         .then(respuesta=>respuesta.json())
-        .then( (datosRespuesta) => {console.log(datosRespuesta)})
+        .then( (datosRespuesta) => {
+
+                console.log(datosRespuesta);
+                this.setState({ datosCargados:true })
+            })
         .catch( console.log)
     }
 
@@ -21,29 +25,36 @@ class Listar extends React.Component {
     }
 
     render() { 
-        return ( <table className="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Correo</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td >1</td>
-                    <td>Oscar</td>
-                    <td>oscar@develoteca.com</td>
-                    <td>
-                        <div className="btn-group" role="group" aria-label="">
-                            <Link className="btn btn-warning" to={"/editar"}>Editar</Link>
-                            <button type="button" className="btn btn-danger">Borrar</button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table> );
+        const{datosCargados}=this.state
+
+        if(!datosCargados){
+            return (<div>Cargando...</div>);
+        }
+        else{
+            return ( <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Correo</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td >1</td>
+                        <td>Oscar</td>
+                        <td>oscar@develoteca.com</td>
+                        <td>
+                            <div className="btn-group" role="group" aria-label="">
+                                <Link className="btn btn-warning" to={"/editar"}>Editar</Link>
+                                <button type="button" className="btn btn-danger">Borrar</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table> );
+        }
     }
 }
 export default Listar;
