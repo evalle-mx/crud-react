50069e8bea14e49b1a64724370ced80ee2906eed
Ernesto V <evalle.mx@gmail.com>
2021-10-13 21:46:06 -0400

    21) Mostrando datos para editar

--- src/componentes/Editar.js
+++ src/componentes/Editar.js
@@ -3,21 +3,34 @@ import React from 'react';
 class Editar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { datosCargados:false,
+        empleado:[] }
     }
 
     componentDidMount(){
         console.log(this.props.match.params.id);
+
+        fetch("http://127.0.0.1/empleados/?consultar="+this.props.match.params.id)
+        .then(respuesta=>respuesta.json())
+        .then( (datosRespuesta) => {
+
+                console.log('=>'+datosRespuesta);
+                this.setState({ datosCargados:true, empleado:datosRespuesta[0]})
+            })
+        .catch( console.log)
     }
 
     render() { 
+        const{datosCargados, empleado }=this.state
+
         return ( <div className="card">
             <div className="card-header">
                 Editar Empleados
             </div>
             <div className="card-body">
-                <h4 className="card-title">Title</h4>
-                <p className="card-text">Text</p>
+                {empleado.id} <br/>
+                {empleado.nombre}<br/> 
+                {empleado.correo}
             </div>
             <div className="card-footer text-muted">
                 Footer
