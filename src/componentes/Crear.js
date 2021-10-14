1b87fef19dc60f4a708d59386d4312970241e090
Ernesto V <evalle.mx@gmail.com>
2021-10-13 21:02:57 -0400

    16) Formulario insertar empleado

--- src/componentes/Crear.js
+++ src/componentes/Crear.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 
 class Crear extends React.Component {
     constructor(props) {
@@ -11,11 +13,29 @@ class Crear extends React.Component {
                 Empleados
             </div>
             <div className="card-body">
-                <h4 className="card-title">Title</h4>
-                <p className="card-text">Text</p>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="">Nombre:</label>
+                        <input type="text" name="nombre" id="nombre" className="form-control" placeholder="" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Escribe el nombre del empleado</small>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="correo">Correo:</label>
+                        <input type="text" name="correo" id="correo" className="form-control" placeholder="user@mail.com" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Correo del empleado</small>
+                    </div>
+
+                    <div className="btn-group" role="group" aria-label="">
+                        <button type="submit" className="btn btn-success">Agregar nuevo empleado</button>
+                        <Link className="btn btn-primary" to={"/"}>Cancelar</Link>
+                    </div>
+
+                </form>
+
             </div>
             <div className="card-footer text-muted">
-                Footer
+                
             </div>
         </div> );
     }
