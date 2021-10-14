24226d3b36aae18286e1dfcf2c38fd80b0ed9709
Ernesto V <evalle.mx@gmail.com>
2021-10-13 21:54:17 -0400

    22) Mostrar datos en formulario

--- src/componentes/Editar.js
+++ src/componentes/Editar.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 class Editar extends React.Component {
     constructor(props) {
         super(props);
         this.state = { datosCargados:false,
         empleado:[] }
+    }
+
+    cambioValor = (e) => {
+        const state = this.state;
+        state[e.target.name]=e.target.value;
+        this.setState({ empleado:state });
+    }
+
+    enviarDatos = (e) => {
+        e.preventDefault();
+        console.log('Formulario enviado...');
     }
 
     componentDidMount(){
@@ -28,9 +40,27 @@ class Editar extends React.Component {
                 Editar Empleados
             </div>
             <div className="card-body">
-                {empleado.id} <br/>
-                {empleado.nombre}<br/> 
-                {empleado.correo}
+
+                <form onSubmit={this.enviarDatos}>
+                    {empleado.id}
+                    <div className="form-group">
+                        <label htmlFor="">Nombre:</label>
+                        <input type="text" name="nombre" id="nombre" value={empleado.nombre} onChange={this.cambioValor} className="form-control" placeholder="" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Escribe el nombre del empleado</small>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="correo">Correo:</label>
+                        <input type="text" name="correo" id="correo" value={empleado.correo} onChange={this.cambioValor} className="form-control" placeholder="user@mail.com" aria-describedby="helpId"/>
+                        <small id="helpId" className="text-muted">Correo del empleado</small>
+                    </div>
+
+                    <div className="btn-group" role="group" aria-label="">
+                        <button type="submit" className="btn btn-success">Agregar nuevo empleado</button>
+                        <Link className="btn btn-primary" to={"/"}>Cancelar</Link>
+                    </div>
+
+                </form>
             </div>
             <div className="card-footer text-muted">
                 Footer
